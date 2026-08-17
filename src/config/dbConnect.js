01818d3179142c101c2afb7b85227d84719ee0f6
RonaldoCesar28 <ronaldocesar28@outlook.com>
2026-08-17 16:28:25 -0300

    acessando a coleção livros

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,7 @@
-import mongoose, {mongo} from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.epee4dn.mongodb.net/livraria?appName=Cluster0");
-
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
     return mongoose.connection;
 };
 
